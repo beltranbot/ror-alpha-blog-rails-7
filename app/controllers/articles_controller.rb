bd644641af20f376cb68dd34a4fe8c43eabb79fb
beltranbot <beltranbot@gmail.com>
2023-11-19 14:14:10 -0500

    finished adding articles functionality with error and success messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,22 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new params.require(:article).permit(:title, :description)
+    if @article.save
+      # notice and alert are used values
+      flash[:notice] = "Article was created successfully."
+      # redirect after creation
+      redirect_to @article
+      # above is shortcut for: redirect_to article_path(@article)
+    else
+      # if it hits a validation error
+      render :new, status: 422
+    end
+  end
+
 end
