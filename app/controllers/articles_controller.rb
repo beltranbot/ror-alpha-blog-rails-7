a0bdbf9b857a6142d7712866830b6a24e4b762f2
beltranbot <beltranbot@gmail.com>
2023-11-19 14:29:27 -0500

    added edit/update article functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find params[:id]
+  end
+
   def create
     @article = Article.new params.require(:article).permit(:title, :description)
     if @article.save
@@ -23,6 +27,17 @@ class ArticlesController < ApplicationController
     else
       # if it hits a validation error
       render :new, status: 422
+    end
+  end
+
+  def update
+    @article = Article.find params[:id]
+    if @article.update params.require(:article).permit(:title, :description)
+      flash[:notice] = "Article was updated successfully."
+      # redirect after creation
+      redirect_to @article
+    else
+      render :edit, status: 422
     end
   end
 
