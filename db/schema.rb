23aef9161fe10ef60f312d6332c15453f6238204
beltranbot <beltranbot@gmail.com>
2023-11-19 23:08:24 -0500

    added email to users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_034043) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_040524) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_034043) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
